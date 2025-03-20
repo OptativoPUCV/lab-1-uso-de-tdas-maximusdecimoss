3c18d5b559fea89531122d0ac5d7d11cd927b6a6
Your Name <you@example.com>
2025-03-20 16:07:38 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -139,16 +139,16 @@ int parentesisBalanceados(char *cadena) {
       } else if (cadena[iteration] == ')') {
          if (top(stack) == NULL) {
             free(stack);
-            return 0;
+            return 1;
          }
          else{
-            return 1;
+            return 0;
          }
          pop(stack);
       }
    }
 
-   int resultado = (top(stack) == NULL);
+   
    free(stack);
    return 1;
 }
